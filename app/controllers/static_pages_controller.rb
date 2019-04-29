fe87edff0f85b4b6f09dd62dc02c5a6abf22e54c
Baptiste Veyrard <baptiste.veyrard@gmail.com>
2019-04-29 13:54:52 +0200

    mpour yoyo

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,8 @@
+class StaticPagesController < ApplicationController
+  def contact
+  end
+
+  def team
+  end
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'static_pages/contact', to: 'static_pages#contact'
+
+  get 'static_pages/team', to: 'static_pages#team'
 end
